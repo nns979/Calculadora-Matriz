9f769689af3f6bcd4d5fa57867410c8794bc3dd9
BingHeng Huang <hk742015245@gmail.com>
2026-04-09 09:43:44 -0700

    Agragar primera version de clase matriz

---
+++ matrices/matriz.py
@@ -0,0 +1,29 @@
+
+class Matriz:
+    def __init__(self, values):
+        size
+        #for i in range(0, len(values))
+
+
+        # 计算矩阵(Matriz)所有的列数是否一样
+        cols = None
+        for row in values:
+            if cols == None:
+                cols = len(row)
+            elif cols != len(row):
+                raise ValueError("La matriz debe tener las mismas columnas en cada fila!")
+            
+        
+        if cols == 0:
+            raise ValueError("Las filas deben tener al menos una columna.")
+        
+        self.cols = cols
+        self.rows = len(values)
+        self.values = values
+        
+        # 获取idx位置的数据
+        def __getitem__(self, idx):
+            if idx < 0 or idx >= self.rows:
+                raise IndexError(f"Indice para fila fuera de rango: ({idx})")
+            
+            return self.values[idx]
